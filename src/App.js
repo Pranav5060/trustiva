930e2165e554db51373215bd211e56f08e811126
Pranav5060 <95734768+Pranav5060@users.noreply.github.com>
2022-08-04 09:17:30 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Carousal from './Components/Carousal';
 import Footer from './Components/Footer';
