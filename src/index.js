b2d63d1ffb21601b5a67c5bab456d0a866363378
Pranav5060 <95734768+Pranav5060@users.noreply.github.com>
2022-08-04 09:14:54 +0530

    routing

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <HashRouter>
     <App />
+  </HashRouter>
   </React.StrictMode>
 );
 
